3e8468a90db217e7bfa6c9ebba8710c38c825fa4
RadKirill00 <Radkirill00@mail.ru>
2024-11-13 13:43:17 +0300

    done configuration config file

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"fmt"
-)		
+  
+)
 
-func main() {				
-	fmt.Println("hello Go")
-} 
+func main() {
+ 
+
+}
+
+
+
 
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,16 @@
-package main
+package config
 
-import (
+import "time"
 
-)
 
-func main() {
-	
+type Config struct {
+	Env string `yaml: "env"`
+	StoragePath string `yaml: "storage" env-required: "true" `
+	HTTPServer `yaml: "http_server"`
+}
+
+type HTTPServer struct {
+	Adress string `yaml: "address" env-default: "localhost:8080"`
+	Timeout time.Duration `yaml: "timeout"`
+	IdleTimeout time.Duration `yaml: "idle-timeout"`
 }
