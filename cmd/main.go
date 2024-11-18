d0d222efcedecd55c348713efb3a224d14f5c322
RadKirill00 <Radkirill00@mail.ru>
2024-11-18 20:33:14 +0300

    done logger

--- cmd/main.go
+++ cmd/main.go
@@ -21,15 +21,33 @@ func main() {
 	}
 	fmt.Println(cfg)
 
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting", slog.String("env", cfg.Env))
+	log.Debug("Debug messages are enabled")
+
 }
 
 
 
-func setupLogger(env string) {
+func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 
 	switch env {
-	case envLocal:  
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		case envLocal:  
+			log = slog.New(
+				slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+			)
+		case envDev: {
+			log = slog.New(
+				slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+			)
+		}
+		case envProd: {
+			log = slog.New(
+				slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+			)
+		}
 	}
+	return log
 }
